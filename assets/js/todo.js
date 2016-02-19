8c195d06ab15f5cc652021533646223c8ac597c8
Stephen Gray <DevelopGray@gmail.com>
2016-02-19 12:08:51 -0600

    Used toggleClass
    Changed the JS to use toggleClass instead of if statement.

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,17 +1,4 @@
 // Check off todos by clicking
 $("li").click(function(){
-	// if li is gray
-	if ($(this).css("color") === "rgb(128, 128, 128)") {
-		// Turn it black
-		$(this).css({
-			color: "black",
-			textDecoration: "none"
-		});
-	} else {
-		// turn it gray
-		$(this).css({
-			color: "gray",
-			textDecoration: "line-through"
-		});
-	}
+	$(this).toggleClass("completed");
 });
