112ce684e41195db503df802d738a0bce78df59c
Stephen Gray <DevelopGray@gmail.com>
2016-02-19 12:05:43 -0600

    Check off effect
    Finished check off and on effect in JS.

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,7 +1,17 @@
 // Check off todos by clicking
 $("li").click(function(){
-	$(this).css({
-		color: "gray",
-		textDecoration: "line-through"
-	});
+	// if li is gray
+	if ($(this).css("color") === "rgb(128, 128, 128)") {
+		// Turn it black
+		$(this).css({
+			color: "black",
+			textDecoration: "none"
+		});
+	} else {
+		// turn it gray
+		$(this).css({
+			color: "gray",
+			textDecoration: "line-through"
+		});
+	}
 });
